1b80d63bec39d965fe3d4235957a06f1932f4d86
minakdan <danilminakov33@gmail.com>
2026-04-03 00:53:47 +0200

    delete comments

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -4,13 +4,9 @@ import (
 	"net/http"
 )
 
-// RegisterRoutes собирает все обработчики в один роутер (Mux).
-// Мы возвращаем http.Handler — это стандартный интерфейс для всех роутеров в Go.
 func RegisterUserRoutes(userH *UsersHandler) http.Handler {
 	mux := http.NewServeMux()
 
-	// Группируем роуты здесь. Если завтра решишь сменить префикс на /api/v1,
-	// ты сделаешь это в одном месте.
 	mux.HandleFunc("POST /users/register", userH.CreateUser)
 	mux.HandleFunc("POST /users/login", userH.LoginUser)
 	mux.HandleFunc("GET /user/profile/{user_id}", userH.GetProfile)
